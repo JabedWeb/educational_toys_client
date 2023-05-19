ae77cc5d4d320cb66ff9b80ba2e3e4c35f89d97e
jabed366 <jabedhasan231@gmail.com>
2023-05-20 03:33:11 +0600

    Modal create for update toys

---
+++ src/components/Modal/MyVerticallyCenteredModal.jsx
@@ -0,0 +1,114 @@
+import { Form } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import './toysModal.css'
+import { useContext, useState } from 'react';
+import { authContext } from '../../providers/authprovider/AuthProvider';
+
+const MyVerticallyCenteredModal = ({show,toyid,toy,onHide,myToys,setMyToys}) => {
+  console.log(toyid,toy);
+  const {user}=useContext(authContext);
+  const handleSubmitUpdate = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const sellerName = form.sellerName.value;
+    const sellerEmail = form.sellerEmail.value;
+    const subcategory = form.subcategory.value;
+    const price = form.price.value;
+    const rating = form.rating.value;
+    const quantity = form.quantity.value;
+    const description = form.description.value;
+    const pictureUrl = form.pictureUrl.value;
+    const updateToy = {
+        name,
+        sellerName,
+        sellerEmail,
+        subcategory,
+        price,
+        rating,
+        quantity,
+        description,
+        pictureUrl
+    }
+    console.log(updateToy);
+    //update the data to the server
+    fetch(`http://localhost:5000/toys/${toyid}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(updateToy)
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        form.reset();
+        // if(data.modifiedCount>0){
+        //   //update the state
+        //   const remining=myToys.filter(toyy=>toyy._id!==toyid);
+        //   console.log("remining",remining);
+        //   const updated=myToys.find(toyy=>toyy._id==toyid);
+        //   console.log("updated",updated);
+        //   const newToys=[updated,...remining];
+        //   console.log("newToys",newToys);
+        //   setMyToys(newToys);
+        // }
+        onHide();
+    })
+}
+
+  return (
+<Modal show={show} onHide={onHide}>
+      <Modal.Header closeButton>
+        <Modal.Title>Add a Toy</Modal.Title>
+      </Modal.Header>
+    <Modal.Body>
+    <Form onSubmit={handleSubmitUpdate}>
+      <Form.Group controlId="name">
+        <Form.Label>Name:</Form.Label>
+        <Form.Control type="text" name="name" defaultValue ={toy?.name} />
+      </Form.Group>
+      <Form.Group controlId="sellerName">
+        <Form.Label>Seller Name:</Form.Label>
+        <Form.Control type="text" name="sellerName" defaultValue={user?.name} />
+      </Form.Group>
+      <Form.Group controlId="sellerEmail">
+        <Form.Label>Seller Email:</Form.Label>
+        <Form.Control type="email" name="sellerEmail" defaultValue={user?.email} />
+      </Form.Group>
+      <Form.Group controlId="subcategory">
+        <Form.Label>Subcategory:</Form.Label>
+        <Form.Control type="text" name="subcategory" defaultValue={toy.subcategory}/>
+      </Form.Group>
+      <Form.Group controlId="price">
+        <Form.Label>Price:</Form.Label>
+        <Form.Control type="number" name="price" defaultValue={toy.price} />
+      </Form.Group>
+      <Form.Group controlId="rating">
+        <Form.Label>Rating:</Form.Label>
+        <Form.Control type="number" name="rating" defaultValue={toy.rating} />
+      </Form.Group>
+      <Form.Group controlId="quantity">
+        <Form.Label>Quantity:</Form.Label>
+        <Form.Control type="number" name="quantity" defaultValue={toy.quantity}/>
+      </Form.Group>
+      <Form.Group controlId="description">
+        <Form.Label>Description:</Form.Label>
+        <Form.Control as="textarea" name="description" defaultValue={toy.description} />
+      </Form.Group>
+      <Form.Group controlId="pictureUrl">
+        <Form.Label>Picture URL:</Form.Label>
+        <Form.Control type="text" name="pictureUrl" defaultValue={toy.pictureUrl}/>
+      </Form.Group>
+      <Button  className='my-3 mx-auto w-100' type="submit">Submit</Button>
+    </Form>
+    </Modal.Body>
+    <Modal.Footer>
+      <Button onClick={onHide}>Close</Button>
+    </Modal.Footer>
+  </Modal>
+  )
+}
+
+export default MyVerticallyCenteredModal
--- src/pages/MyToys/MyToys.jsx
+++ src/pages/MyToys/MyToys.jsx
@@ -2,17 +2,12 @@ import React, { useContext, useEffect, useState } from 'react';
 import { Container, Table, Button} from 'react-bootstrap';
 //import { useHistory } from 'react-router-dom';
 import { authContext } from '../../providers/authprovider/AuthProvider';
+import MyVerticallyCenteredModal from '../../components/Modal/MyVerticallyCenteredModal';
 
 const MyToys = () => {
   const { user } = useContext(authContext);
   const [myToys, setMyToys] = useState([]);
-//   const [showModal, setShowModal] = useState(false);
-//   const [selectedToy, setSelectedToy] = useState(null);
-//   const [updateFormData, setUpdateFormData] = useState({
-//     price: '',
-//     quantity: '',
-//     description: '',
-//   });
+  const [modalShow, setModalShow] = useState(false);
 //   const history = useHistory();
 
   const url = `http://localhost:5000/mytoys?email=${user.email}`;
@@ -27,7 +22,7 @@ const MyToys = () => {
       .catch((error) => {
         console.log(error);
       });
-  }, []);
+  }, [myToys]);
 
   const handleDeleteToy = (toyId) => {
     const confirmed = window.confirm('Are you sure you want to delete this toy?');
@@ -47,64 +42,6 @@ const MyToys = () => {
         });
     }
   };
-
-  const handleUpdateToy = (toyId) => {
-    const toyToUpdate = myToys.find((toy) => toy._id === toyId);
-    // setSelectedToy(toyToUpdate);
-    // setUpdateFormData({
-    //   price: toyToUpdate.price,
-    //   quantity: toyToUpdate.quantity,
-    //   description: toyToUpdate.description,
-    // });
-    // setShowModal(true);
-  };
-
-//   const handleUpdateFormChange = (e) => {
-//     setUpdateFormData({ ...updateFormData, [e.target.name]: e.target.value });
-//   };
-
-//   const handleUpdateFormSubmit = (e) => {
-//     e.preventDefault();
-//     const updatedToy = {
-//       ...selectedToy,
-//       price: updateFormData.price,
-//       quantity: updateFormData.quantity,
-//       description: updateFormData.description,
-//     };
-
-    // Send request to update the toy
-//     fetch(`http://localhost:5000/toys/${selectedToy._id}`, {
-//       method: 'PUT',
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//       body: JSON.stringify(updatedToy),
-//     })
-//       .then((res) => res.json())
-//       .then((data) => {
-//         console.log('Toy updated successfully');
-//         // Update the toy in the toy list
-//         setMyToys(myToys.map((toy) => (toy._id === selectedToy._id ? updatedToy : toy)));
-//         handleCloseModal();
-//       })
-//       .catch((error) => {
-//         console.log(error);
-//       });
-//   };
-
-//   const handleCloseModal = () => {
-//     setShowModal(false);
-//     setSelectedToy(null);
-//     setUpdateFormData({
-//       price: '',
-//       quantity: '',
-//       description: '',
-//     });
-//   };
-
-//   const handleAddToy = () => {
-//     history.push('/add-toy');
-//   };
 
   return (
     <Container>
@@ -126,9 +63,18 @@ const MyToys = () => {
               <td>{toy.quantity}</td>
               <td>{toy.description}</td>
               <td>
-                <Button variant="info" onClick={() => handleUpdateToy(toy._id)}>
-                  Update
-                </Button>{' '}
+                <Button variant="primary" onClick={() => setModalShow(true)}>
+                    Update
+                </Button>
+
+                <MyVerticallyCenteredModal
+                    show={modalShow}
+                    toyid={toy._id}
+                    toy={toy}
+                    myToys={myToys}
+                    setMyToys={setMyToys}
+                    onHide={() => setModalShow(false)}
+                />
                 <Button variant="danger" onClick={() => handleDeleteToy(toy._id)}>
                   Delete
                 </Button>
@@ -138,50 +84,7 @@ const MyToys = () => {
         </tbody>
       </Table>
 
-      {/* <Modal show={showModal} onHide={handleCloseModal}>
-        <Modal.Header closeButton>
-          <Modal.Title>Update Toy</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form onSubmit={handleUpdateFormSubmit}>
-            <Form.Group controlId="formPrice">
-              <Form.Label>Price</Form.Label>
-              <Form.Control
-                type="number"
-                name="price"
-                value={updateFormData.price}
-                onChange={handleUpdateFormChange}
-              />
-            </Form.Group>
-            <Form.Group controlId="formQuantity">
-              <Form.Label>Quantity</Form.Label>
-              <Form.Control
-                type="number"
-                name="quantity"
-                value={updateFormData.quantity}
-                onChange={handleUpdateFormChange}
-              />
-            </Form.Group>
-            <Form.Group controlId="formDescription">
-              <Form.Label>Description</Form.Label>
-              <Form.Control
-                as="textarea"
-                name="description"
-                value={updateFormData.description}
-                onChange={handleUpdateFormChange}
-              />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Update
-            </Button>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleCloseModal}>
-            Close
-          </Button>
-        </Modal.Footer>
-      </Modal> */}
+  
 
     </Container>
   );
